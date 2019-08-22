775f926e34924dd4878807359a898e69041cc4ea
Erik Uy <eriuy@publicisgroupe.net>
2019-08-22 12:57:45 +1000

    added vuex and vue-rotue

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  router,
+  store,
   render: h => h(App)
 }).$mount('#app')
